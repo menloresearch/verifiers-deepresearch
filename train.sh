102d11ab3e8073fc35f4dc96d8ce0d207a17fe3e
nguyenhoangthuan99 <alex@menlo.ai>
2025-06-19 10:17:40 +0000

    Init docker usage for training

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 # export NCCL_P2P_DISABLE=0
 CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 accelerate launch --config-file configs/zero3.yaml --num_processes 6 verifiers/examples/trl_no_think_v2.py \
-    --async_generation_timeout 3600
+    --async_generation_timeout 3600 --vllm_server_host inference  
+    
